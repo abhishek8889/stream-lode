8299c08e7ac6e270c1087a8493ba90b381a03c67
DeveloperItExpert <developeritexpert3@gmail.com>
2023-03-06 15:22:27 +0530

    updates

--- resources/views/Host/calendar/index2.blade.php
+++ resources/views/Host/calendar/index2.blade.php
@@ -8,7 +8,7 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle">Enter your availabele time </h5>
+              <h5 class="modal-title" id="exampleModalLongTitle">Enter your available time </h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close" id="calendarCloseBtn">
                 <span aria-hidden="true">&times;</span>
               </button>
